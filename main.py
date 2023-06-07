3130d2ef259b425276b4064786d2eeb1265dfe38
Kevnnnm <17kevinguo@gmail.com>
2023-06-07 09:25:00 -0500

    added basic documentation

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import torch
 from torch import nn, optim
 from torch.utils.data import DataLoader
 from torchvision import datasets
@@ -15,11 +16,20 @@ criterion = nn.NLLLoss()
 optimizer = optim.SGD(model.parameters(), lr=0.003, momentum=0.9)
 epochs = 10
 
+#train and test the split learning model
 classes.train(epochs, model, trainloader, criterion, optimizer)
 classes.test(model, testloader)
 
+#train the attacker (malicious server)
 attack = classes.Attacker().to('mps')
 attack_loader = datas.loaders['attack']
 final_loader = datas.final_loaders['test']
 optimizer_attack = optim.Adam(attack.parameters(), lr = 1e-3)
 classes.attack(epochs, attack, model, optimizer_attack, attack_loader, testloader, final_loader)
+
+#save models
+torch.save(model.state_dict(), "SplitNN_model.pth")
+print("Saved PyTorch Model State to SplitNN_model.pth")
+
+torch.save(attack.state_dict(), "attack_1_model.pth")
+print("Saved PyTorch Model State to attack_1_model.pth")
